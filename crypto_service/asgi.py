2188c6b4a256ca201b7964b61f1d53c75b7d245f
Shayan <shayansadeghi178@gmail.com>
2022-04-11 13:57:17 +0430

    customize asgi file for add websocket protocol type router

---
+++ crypto_service/asgi.py
@@ -0,0 +1,27 @@
+"""
+ASGI config for crypto_service project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
+"""
+
+import os
+
+from channels.auth import AuthMiddlewareStack
+from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+
+from one_min_price.routing import ws_urlpatterns
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'crypto_service.settings')
+
+application = ProtocolTypeRouter({
+    'http': get_asgi_application(),
+    'websocket': AuthMiddlewareStack(
+        URLRouter(
+            ws_urlpatterns
+        ),
+    )
+})
