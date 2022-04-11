6f81fc78753d38b736e4ab36f05eb982eb2428dc
Shayan <shayansadeghi178@gmail.com>
2022-04-11 14:14:09 +0430

    create up down task

---
+++ one_min_price/tasks.py
@@ -0,0 +1,15 @@
+from celery import shared_task
+from binance import Client
+from datetime import datetime
+
+
+@shared_task
+def up_down(symbol: str, price: float):
+    client = Client()
+    last_candle = client.get_klines(symbol=symbol, interval=Client.KLINE_INTERVAL_1MINUTE, limit=1)[0]
+    candle_price = float(last_candle[4])
+    candle_timestamp = datetime.fromtimestamp(float(last_candle[0]) / 1000)
+    if candle_price > price:
+        return f'time:{candle_timestamp} --- Up'
+    else:
+        return f'time:{candle_timestamp} --- Down'
